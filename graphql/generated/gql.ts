6a86b19658fce16f7a0c6562cbe2149aad977833
saku-1101 <sakupi1101@outlook.jp>
2023-04-24 17:14:51 +1000

    [del]client-presetの導入によるプラグイン削除

--- graphql/generated/gql.ts
+++ graphql/generated/gql.ts
@@ -13,7 +13,8 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
-    "\n    query getStatus {\n      statuses {\n        author\n        body\n        createdAt\n        id\n      }\n    }\n  ": types.GetStatusDocument,
+    "mutation CreateUser {\n  createUser(data: {name: \"hoge\", email: \"foo@hoge.co.jp\"}) {\n    name\n  }\n}": types.CreateUserDocument,
+    "query getStatus {\n  statuses {\n    author\n    body\n    createdAt\n    id\n  }\n}": types.GetStatusDocument,
 };
 
 /**
@@ -33,7 +34,11 @@ export function graphql(source: string): unknown;
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n    query getStatus {\n      statuses {\n        author\n        body\n        createdAt\n        id\n      }\n    }\n  "): (typeof documents)["\n    query getStatus {\n      statuses {\n        author\n        body\n        createdAt\n        id\n      }\n    }\n  "];
+export function graphql(source: "mutation CreateUser {\n  createUser(data: {name: \"hoge\", email: \"foo@hoge.co.jp\"}) {\n    name\n  }\n}"): (typeof documents)["mutation CreateUser {\n  createUser(data: {name: \"hoge\", email: \"foo@hoge.co.jp\"}) {\n    name\n  }\n}"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "query getStatus {\n  statuses {\n    author\n    body\n    createdAt\n    id\n  }\n}"): (typeof documents)["query getStatus {\n  statuses {\n    author\n    body\n    createdAt\n    id\n  }\n}"];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
--- graphql/generated/graphql.ts
+++ graphql/generated/graphql.ts
@@ -14,6 +14,16 @@ export type Scalars = {
   Float: number;
 };
 
+export type Mutation = {
+  __typename?: 'Mutation';
+  createUser: User;
+};
+
+
+export type MutationCreateUserArgs = {
+  data: UserCreateInput;
+};
+
 export type Query = {
   __typename?: 'Query';
   hello: Scalars['String'];
@@ -28,37 +38,29 @@ export type Status = {
   id: Scalars['String'];
 };
 
+export type User = {
+  __typename?: 'User';
+  email: Scalars['String'];
+  id: Scalars['ID'];
+  name: Scalars['String'];
+};
+
+export type UserCreateInput = {
+  email: Scalars['String'];
+  id?: InputMaybe<Scalars['ID']>;
+  name: Scalars['String'];
+};
+
+export type CreateUserMutationVariables = Exact<{ [key: string]: never; }>;
+
+
+export type CreateUserMutation = { __typename?: 'Mutation', createUser: { __typename?: 'User', name: string } };
+
 export type GetStatusQueryVariables = Exact<{ [key: string]: never; }>;
 
 
 export type GetStatusQuery = { __typename?: 'Query', statuses: Array<{ __typename?: 'Status', author: string, body: string, createdAt: string, id: string } | null> };
 
 
+export const CreateUserDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"CreateUser"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"createUser"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"data"},"value":{"kind":"ObjectValue","fields":[{"kind":"ObjectField","name":{"kind":"Name","value":"name"},"value":{"kind":"StringValue","value":"hoge","block":false}},{"kind":"ObjectField","name":{"kind":"Name","value":"email"},"value":{"kind":"StringValue","value":"foo@hoge.co.jp","block":false}}]}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}}]}}]}}]} as unknown as DocumentNode<CreateUserMutation, CreateUserMutationVariables>;
 export const GetStatusDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"getStatus"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"statuses"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"author"}},{"kind":"Field","name":{"kind":"Name","value":"body"}},{"kind":"Field","name":{"kind":"Name","value":"createdAt"}},{"kind":"Field","name":{"kind":"Name","value":"id"}}]}}]}}]} as unknown as DocumentNode<GetStatusQuery, GetStatusQueryVariables>;
-/** All built-in and custom scalars, mapped to their actual values */
-export type Scalars = {
-  ID: string;
-  String: string;
-  Boolean: boolean;
-  Int: number;
-  Float: number;
-};
-
-export type Query = {
-  __typename?: 'Query';
-  hello: Scalars['String'];
-  statuses: Array<Maybe<Status>>;
-};
-
-export type Status = {
-  __typename?: 'Status';
-  author: Scalars['String'];
-  body: Scalars['String'];
-  createdAt: Scalars['String'];
-  id: Scalars['String'];
-};
-
-export type GetStatusQueryVariables = Exact<{ [key: string]: never; }>;
-
-
-export type GetStatusQuery = { __typename?: 'Query', statuses: Array<{ __typename?: 'Status', author: string, body: string, createdAt: string, id: string } | null> };
