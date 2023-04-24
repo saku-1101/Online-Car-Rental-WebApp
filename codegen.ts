a2ec21d7ad87133b28a1bc9fcb67f415e6a3e907
saku-1101 <sakupi1101@outlook.jp>
2023-04-24 17:13:34 +1000

    [fix]オフラインでもcodegenできるスキーマ設定．client-presetを使用しているため使用しているためプラグイン不要．前コミットの関心わけによる読み込むdocumentの変更’

--- codegen.ts
+++ codegen.ts
@@ -2,12 +2,15 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'http://localhost:3000/api/graphql', // apiエンドポイント
-  documents: ['./pages/*.tsx'], // clientサイドでクエリが書かれている場所
+  schema: 'graphql/schemas/schema.graphql', // apiエンドポイント
+  documents: ['./graphql/**/*.graphql'],
   generates: {
     './graphql/generated/': {
       preset: 'client',
-      plugins: ['typescript', 'typescript-operations'],
+      plugins: [], // client-presetに含まれているのでプラグインは使用しない(*使用する場合，重複した型定義がなされる）
+      config: {
+        rawRequest: true,
+      },
     },
     './graphql.schema.json': {
       plugins: ['introspection'],
