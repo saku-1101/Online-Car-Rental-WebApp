e270c26d5deda3b9911a5fd2d2ea644e10515346
saku-1101 <sakupi1101@outlook.jp>
2023-04-28 17:04:36 +1000

    [fix]apollo client が使えなかった件の解消

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client';
 import { _apolloClient } from '@@/apollo/apollo-client';
 
+// pages 以下の全てのページで共通して使うコンポーネント
+// Apollo Clientは現段階ではpages配下でしか呼び出せない模様
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={_apolloClient}>
