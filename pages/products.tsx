2dab06f8dd9a66785f75d22c030ace1dd1efdb06
saku-1101 <sakupi1101@outlook.jp>
2023-05-03 18:14:14 +1000

    [add]pagesたち

---
+++ pages/products.tsx
@@ -0,0 +1,9 @@
+import ProductsPage from '@@/components/templates/ProductsPage';
+
+export default function Products() {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <ProductsPage />
+    </div>
+  );
+}
---
+++ pages/registration.tsx
@@ -0,0 +1,9 @@
+import CustomerRegistration from '@@/components/templates/CustomerRegistration';
+
+export default function Products() {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <CustomerRegistration />
+    </div>
+  );
+}
