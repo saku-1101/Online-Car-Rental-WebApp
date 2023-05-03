2bffae17a6981b361c8b6cc0a08f94772e720c54
saku-1101 <sakupi1101@outlook.jp>
2023-05-03 17:26:45 +1000

    [add]product page

---
+++ components/morecules/ProductCard.tsx
@@ -0,0 +1,28 @@
+import OrderButton from '../atoms/OrderButton';
+import { Car } from '@@/pages/api/front/generated/graphql';
+
+export default function ProductCard(props: { car: Car; button: string }) {
+  return (
+    <>
+      <div className='card card-compact w-full aspect-square bg-base-100 shadow-xl p-0'>
+        <figure className='w-full h-3/4'>
+          {/* <img src={props.product.url} alt='' className='object-cover w-full h-full' /> */}
+        </figure>
+        <div className='card-body'>
+          <h2 className='card-title text-neutral-focus'>
+            {props.car.brand}-{props.car.model}-{props.car.model_year}
+          </h2>
+          <div className='badge badge-secondary'>{props.car.availability ? 'Available' : 'Not Available'}</div>
+          <p className='text-base-content justify-start'>Milage: {props.car.mileage}</p>
+          <p className='text-base-content justify-start'>Fuel Type: {props.car.fuel_type}</p>
+          <p className='text-base-content justify-start'>Price: {props.car.price_per_day}/day</p>
+          <p className='text-base-content justify-start'>Seats: {props.car.seats}</p>
+          <p className='text-base-content justify-start'>Description: {props.car.description}</p>
+          <div className='card-actions justify-end'>
+            <OrderButton label={props.button} car={props.car} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ components/templates/ProductsPage.tsx
@@ -0,0 +1,40 @@
+import ProductCard from '../morecules/ProductCard';
+import PageTitle from '../atoms/PageTitle';
+import { Car, GetCarsDocument } from '@@/pages/api/front/generated/graphql';
+import { useQuery } from '@apollo/client';
+
+export default function ProductsPage() {
+  // TypedDocumentNodeでドキュメントをuseQueryに渡すだけで
+  //　返却されるdataや第二引数など，これ以降の変数に動的に?型付けがされる
+  const { data, loading, error } = useQuery(GetCarsDocument);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    console.error(error);
+    return <div>Error</div>;
+  }
+
+  if (!data) {
+    return <div>No data</div>;
+  }
+
+  // if (loading) return <Loading />;
+  // if (error) return <p>An error occurred</p>;
+  const { cars } = data; // queryの返却値の型が型推論される
+
+  return (
+    <div className='flex flex-col justify-center'>
+      <PageTitle title='Cars' />
+      <div className='flex justify-center'>
+        <div className='w-3/4 grid grid-cols-3 gap-10'>
+          {cars.map((car: Car) => (
+            <ProductCard key={car.car_id} car={car} button='Add to Cart' />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
