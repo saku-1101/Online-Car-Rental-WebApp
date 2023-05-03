236dcf45a36e7c4c0f9a6503777df22668b882f9
saku-1101 <sakupi1101@outlook.jp>
2023-05-03 17:06:09 +1000

    [add]home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,10 @@
 // Client
-import { GetBooksDocument, GetUsersDocument, SayHelloDocument } from '@@/pages/api/front/generated/graphql';
-import { useQuery } from '@apollo/client';
-import { useEffect, useState } from 'react';
+import HomePage from '@@/components/templates/HomePage';
 
 export default function Home() {
-  // TypedDocumentNodeでドキュメントをuseQueryに渡すだけで
-  //　返却されるdataや第二引数など，これ以降の変数に動的に?型付けがされる
-  const [message, setMessage] = useState('');
-  const { data, loading, error } = useQuery(GetUsersDocument);
-  const HandleGetData = async () => {
-    // const message = await useQuery(SayHelloDocument);
-    setMessage('Hora!');
-  };
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    console.error(error);
-    return <div>Error</div>;
-  }
-
-  if (!data) {
-    return <div>No data</div>;
-  }
-
-  // const { books } = data; // ここのbookとかってどうやってわかるんやろ
-  // console.log(data.hello);
-
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
-      <h1>Users</h1>
-      <div>
-        {data.allUsers.map((user, index) => {
-          return (
-            <div key={index}>
-              <p>
-                {user?.id} {user?.name}
-              </p>
-            </div>
-          );
-        })}
-        {message}
-      </div>
-      <button onClick={() => HandleGetData()}>Button</button>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <HomePage />
     </main>
   );
 }
