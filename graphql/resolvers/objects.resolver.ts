ad34ab496f608e060a0f31a2f1f7d9e55f997f9e
saku-1101 <sakupi1101@outlook.jp>
2023-05-03 18:10:36 +1000

    [add]helper

--- graphql/resolvers/objects.resolver.ts
+++ graphql/resolvers/objects.resolver.ts
@@ -1,35 +1,7 @@
 import { _prismaClient as prisma } from './prisma-client';
+import { findCarRentalById, findCustomerRentalById, findPaymentMethodRentalById, findRentalById } from './helper';
 
 import { CarResolvers, CustomerResolvers, PaymentMethodResolvers, RentalResolvers } from '../server/generated/graphql';
-
-async function findRentalById(rentalId: number) {
-  const rental = await prisma.rental.findUnique({ where: { rental_id: rentalId } });
-  if (!rental) {
-    throw new Error(`Rental with ID ${rentalId} not found`);
-  }
-  return rental;
-}
-async function findCustomerRentalById(customerId: number) {
-  const rentals = await prisma.customer.findUnique({ where: { customer_id: customerId } }).rentals();
-  if (!rentals) {
-    throw new Error(`Customer with ID ${customerId} doesn't have rentals`);
-  }
-  return rentals;
-}
-async function findCarRentalById(carId: number) {
-  const rentals = await prisma.car.findUnique({ where: { car_id: carId } }).rentals();
-  if (!rentals) {
-    throw new Error(`Car with ID ${carId} doesn't have rentals`);
-  }
-  return rentals;
-}
-async function findPaymentMethodRentalById(paymentMethodId: number) {
-  const rentals = await prisma.paymentMethod.findUnique({ where: { payment_id: paymentMethodId } }).rentals();
-  if (!rentals) {
-    throw new Error(`Payment method with ID ${paymentMethodId} doesn't have rentals`);
-  }
-  return rentals;
-}
 
 const Rental: RentalResolvers = {
   car: async (parent) => {
