7bbdc6f0666b71e01b5523d762379a946cdbd3a2
saku-1101 <sakupi1101@outlook.jp>
2023-05-03 18:04:20 +1000

    [add]thank you page

---
+++ components/templates/Thankyou.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+
+export default function ThankyouPage() {
+  return (
+    <div className='hero'>
+      <div className='hero-content text-center'>
+        <div className='max-w-md'>
+          <h1 className='text-5xl font-bold'>Thank you for your purchase!</h1>
+          <p className='py-6'>
+            Thank you for shopping with us today! <br></br>
+            We're looking forward to having you soon😊🥕
+          </p>
+          <Link href={{ pathname: '/' }}>
+            <button className='btn btn-primary'>Back to Home</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ pages/thankyou.tsx
@@ -0,0 +1,4 @@
+import Thankyou from '../components/templates/Thankyou';
+export default function ThankyouPage() {
+  return <Thankyou />;
+}
