1c59d0890c61658328b3466694ad96c4fa6efcd9
saku-1101 <sakupi1101@outlook.jp>
2023-05-04 16:51:37 +1000

    [fix]初回レンダリング前にsessionStorageをサーバが読めるようにしておく

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,21 @@ import DraggableCart from '@@/components/atoms/DraggableCart';
 import { RentalsProvider } from '@@/hooks/useRentalContext';
 import NavBar from '@@/components/morecules/NavBar';
 import Footer from '@@/components/atoms/Footer';
+import { usePersistedState } from '@@/hooks/usePersistedState';
+import { Rental } from '@@/hooks/types/rentalContextTypes';
+import router from 'next/router';
+import { useEffect } from 'react';
 
 // pages 以下の全てのページで共通して使うコンポーネント
 // Apollo Clientは現段階ではpages配下でしか呼び出せない模様
 export default function App({ Component, pageProps }: AppProps) {
+  // 初回レンダリング前にsessionStorageをサーバが読めるようにしておく
+  const [customerId, setCustomerId] = usePersistedState<number>({ key: 'customerId', initialValue: 0 });
+  const [rentals, setRentals] = usePersistedState<Rental[]>({ key: 'rentals', initialValue: [] });
+  useEffect(() => {
+    setCustomerId(0);
+    setRentals([]);
+  }, []);
   return (
     <CustomerProvider>
       <RentalsProvider>
